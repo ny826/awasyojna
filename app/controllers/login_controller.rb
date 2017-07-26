787e79949d059741d6249d905a2a4ab51c297e0d
Naveen yadav <yadavnaveen826@gmail.com>
2017-07-26 15:17:06 +0530

    adding login logut and registration api

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,9 +1,11 @@
+require 'httparty'
+require 'json'
+
 class LoginController < ApplicationController
 
-
 def index
-end
 
+end
 
 def vregistration
 end
@@ -17,10 +19,45 @@ def registration
     phone=params[:phone]
     address=params[:address]
     dob=user["born_on"]
+    gender=params[:Gender]
+    father=params[:father]
+    spouse=params[:spouse]
+    pan=params[:pan]
+    aadhar=params[:aadhar]
+    
+    if gender=="Male"
+    no=1;
+    elsif gender=="Female"
+    no=2;
+    elsif gender=="Others"
+    no=3;
+    end	
+    # byebug;
+    @result = HTTParty.post("http://127.0.0.1:8000/user/register",
+         :body => {
+         	:birth_date => dob,
+         	:phone => phone, 
+         	:name => name, 
+         	:email => email, 
+         	:password => password,
+         	:address=>address,
+         	:gender=>no,
+            :father_name=>father,
+            :spouse_name=>spouse,
+            :pan_number=>pan,
+            :aadhar_no=>aadhar,
+         	}.to_json,
+            :headers => { 'Content-Type' => 'application/json' })
 
-    puts "name is #{name}  ,  email is #{email}  ,password is #{password}  ,phone is #{phone}  ,address is #{address}  , dob is #{dob} "
-	return redirect_to '/';
-	
+        puts @result
+
+        if @result["error"]==false
+        return redirect_to '/';
+        else
+        puts "Wrong Entry of inputs "
+        return redirect_to '/vregistration'
+        end
+       
 end
 
 
@@ -32,9 +69,37 @@ def login
 	email=user["address"]
 	password=params[:password]
 
-	return redirect_to '/'
+    @result = HTTParty.post("http://127.0.0.1:8000/user/login",
+         :body => {
+         	:email => email,         	
+         	:password => password,
+         	}.to_json,
+            :headers => { 'Content-Type' => 'application/json' })
+        
+        puts @result
+
+        byebug;
+        if @result["error"]==false
+        session[:user_id]=@result["session_id"]
+        return redirect_to '/';
+        else
+        return redirect_to '/vlogin'
+        end
 end
 
+def logout
+@result = HTTParty.post("http://127.0.0.1:8000/user/logout",
+         :body => {
+         	:session_id=>session[:user_id]
+         	}.to_json,
+            :headers => { 'Content-Type' => 'application/json' })
+        
+        puts @result
+        if @result["error"]==false
+        return redirect_to '/';
+        
+        end
+	return redirect_to '/'end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ get '/vregistration'=>'login#vregistration'
 
 post '/registration'=>'login#registration'
 
+post '/login'=>'login#login'
+post '/logout'=>'login#logout'
+
 get '/vlogin'=>'login#vlogin'
 end
