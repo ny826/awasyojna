4d0c0049dc7a39ce097b75a0f7b75605e2ffd501
manishyadavv <manishyadav.nd@gmail.com>
2017-07-27 21:49:20 +0530

    changed basic layout

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,28 @@
+class HomeController < ApplicationController
+  def hometab
+  end
+
+  def ray
+  end
+
+  def smartcity
+  end
+
+  def location
+  end
+
+  def societyreg
+  end
+
+  def download
+  end
+
+  def tnc
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
 
+  get '/home/hometab' => 'home#hometab'
+
+  get '/home/ray' =>  'home#ray'
+
+  get '/home/smartcity' => 'home#smartcity'
+
+  get '/home/location' => 'home#location'
+
+  get '/home/societyreg' => 'home#societyreg'
+
+  get '/home/download' => 'home#download'
+
+  get '/home/tnc' => 'home#tnc'
+
+  get '/home/blog' => 'home#blog'
+
+  get '/home/contact' => 'home#contact'
+
 get '/'=>'login#index'
 
 get '/vregistration'=>'login#vregistration'
