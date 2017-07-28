b36503fe938422e0b6fc44d617c6971e397c5811
Naveen yadav <yadavnaveen826@gmail.com>
2017-07-28 17:45:01 +0530

    Adding Scheme get api

--- app/controllers/schemes_controller.rb
+++ app/controllers/schemes_controller.rb
@@ -1,7 +1,18 @@
+require 'httparty'
+require 'json'
+
 class SchemesController < ApplicationController
   
 
   def scheme
+         result = HTTParty.get("http://127.0.0.1:8000/scheme/scheme_get")
+         #puts result
+         #puts result["schemes"]
+         result=result["result"]
+         #puts result
+         @res=result["schemes"]
+         #puts @res
+
   end
   
   def scheme_register
