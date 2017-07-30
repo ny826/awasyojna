5d60d232d3191b65311745b825143adbcae98ead
manishyadavv <manishyadav.nd@gmail.com>
2017-07-30 10:56:01 +0530

    manish files

--- test/controllers/schemes_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SchemesControllerTest < ActionDispatch::IntegrationTest
-  test "should get scheme" do
-    get schemes_scheme_url
-    assert_response :success
-  end
-
-end
