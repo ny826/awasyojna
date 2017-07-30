001e087e81d0a9ddb8c6d88ea4fb16d56ba73716
manishyadavv <manishyadav.nd@gmail.com>
2017-07-30 14:54:29 +0530

    naveen

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -64,7 +64,7 @@ def file_upload
 
     if(@result["error"]==false)
     puts @result["error"]
-    return redirect_to '/homw/index'
+    return redirect_to '/home/index'
     else
     puts @result["error"]
     return redirect_to '/index'
